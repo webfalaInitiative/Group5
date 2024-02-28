99b7d3fe9a272acb74f4aeacb2e9b7258cd30123
Ada-nna <cyndigal2@gmail.com>
2024-02-28 16:57:15 +0100

    done

---
+++ app.js
@@ -0,0 +1,14 @@
+const menuBar = document.querySelector(".menu-bar");
+const popUpBox = document.querySelector(".popup-box");
+
+
+var nav = document.querySelector('.goon')
+document.querySelector('.fa-bars').onclick = function(){
+    nav.style.top = '0'
+}
+
+document.querySelector('.fa-close').onclick = function(){
+    nav.style.top = '-500px'
+}
+
+
